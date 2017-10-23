624c5ecdd3f18e4e0df8553bc7d5ea914fd686ca
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-23 14:31:18 -0600

    update file structure with Node and Trie class files and index in root

---
+++ index.js
@@ -0,0 +1,14 @@
+function filterMe (array) {
+  return array
+}
+
+export default filterMe
+
+
+const Node = require('./lib/Node.js');
+const Trie = require('./lib/Trie.js');
+
+module.exports = {
+  Node,
+  Trie
+};
