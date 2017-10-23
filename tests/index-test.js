f064af2607ae606e26bbcf86877e720376d6aa46
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-23 14:09:45 -0600

    test using bubbleSort is passing

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,5 +1,10 @@
 import { expect } from 'chai';
 import index from '../lib/index'
 
+import {bubbleSort, insertionSort, mergeSort, quickSort} from '@katiescruggs/sorting-suite';
+
 describe('Example Test File', () => {
+  it('should successfully use sorts we imported', function() {
+    expect(bubbleSort([3, 2, 1])).to.deep.equal([1,2,3]);
+  })
 })
