9072f4fd78f968ff55a5a6426a425ef834c44b73
katiescruggs <katie.e.scruggs@gmail.com>
2017-10-25 22:00:56 -0600

    add popularity test and make functions consistently arrow functions

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -16,17 +16,20 @@ describe('NODE', () => {
     expect(node.letter).to.equal(null);
   });
 
-  it('should have a default children property of empty object', function() {
+  it('should have a default children property of empty object', () => {
     expect(node.children).to.deep.equal({});
   });
 
-  it('should have a default wordEnd property of false', function() {
+  it('should have a default wordEnd property of false', () => {
     expect(node.wordEnd).to.equal(false);
+  });
+
+  it('should have a default popularity property of 0', () => {
+    expect(node.popularity).to.equal(0);
   });
 
   it('should take in a letter', () => {
     node = new Node('a');
     expect(node.letter).to.equal('a');
   });
-
 });
